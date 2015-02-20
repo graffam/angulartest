7ebcd06432d378a4442d2b97814c780bb93a9bc4
graffam <graffam87@gmail.com>
2015-02-20 13:10:08 -0500

    updates test to show only ones that need bording

--- rails/spec/features/user_views_dogs_spec.rb
+++ rails/spec/features/user_views_dogs_spec.rb
@@ -5,17 +5,19 @@ feature "user views all of the dogs" do
   # I want to be able to view all of the dogs that need to be watched
   # So I may contact an owner to watch their dog.
   # Acceptance Criteria:
-  # [] I must see a list of all the dogs that need to be watched
+  # [X] I must see a list of all the dogs that need to be watched
   # [] I should not see dogs that don't need to be currently watched
 
   before (:each) do
     @dog = FactoryGirl.create(:dog)
+    @dog2 = FactoryGirl.create(:dog, bording: true)
   end
 
- it "sees a list of all the dogs that currently need to be watched" do
+ it "sees a list of all the dogs that currently need to be borded" do
    visit "/dogs"
 
-   expect(page).to have_content(@dog.name)
+   expect(page).to have_content(@dog2.name)
+   expect(page).to_not have_content(@dog.name)
 
  end
 end
